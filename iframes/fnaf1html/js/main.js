cb7114b56628e8cee98a03e7d470d946fd64d048
rorysickgames <ballbuster4827@gmail.com>
2026-09-05 14:04:48 -0500

    Add initDevMover to main.js initialization

--- iframes/fnaf1html/js/main.js
+++ iframes/fnaf1html/js/main.js
@@ -2,10 +2,12 @@ import { initOffice } from './ui/office.js';
 import { initDoors } from './ui/doors.js';
 import { initPowerManager } from './core/powerManager.js';
 import { initCameras } from './ui/cameras.js';
+import { initDevMover } from './core/devMode.js'; // ADD THIS
 
 document.addEventListener('DOMContentLoaded', () => {
     initOffice();
     initDoors();
     initPowerManager();
     initCameras();
+    initDevMover(); // ADD THIS
 });

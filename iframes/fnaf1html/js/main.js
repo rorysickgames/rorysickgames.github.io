950319a86ad880e9f5218393cbd0955433346b88
rorysickgames <ballbuster4827@gmail.com>
2026-09-05 14:04:58 -0500

    Update main.js

--- iframes/fnaf1html/js/main.js
+++ iframes/fnaf1html/js/main.js
@@ -2,12 +2,12 @@ import { initOffice } from './ui/office.js';
 import { initDoors } from './ui/doors.js';
 import { initPowerManager } from './core/powerManager.js';
 import { initCameras } from './ui/cameras.js';
-import { initDevMover } from './core/devMode.js'; // ADD THIS
+import { initDevMover } from './core/devMode.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initOffice();
     initDoors();
     initPowerManager();
     initCameras();
-    initDevMover(); // ADD THIS
+    initDevMover();
 });
